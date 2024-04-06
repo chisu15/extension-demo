76a7bb13a2f7dd0b81a7a34529a1eef8588e7037
chisu15 <trunghieudo1509@gmail.com>
2024-04-06 15:32:51 +0700

    v1

---
+++ popup.js
@@ -0,0 +1,165 @@
+const listDoodle = [{
+    name: "doodle-nut",
+    link: "https://anonyviet.com/wp-content/uploads/2021/05/Game-Doodle-google.jpg"
+  },
+  {
+    name: "doodle-drogoon",
+    link: "https://www.google.com/logos/doodles/2023/celebrating-lake-xochimilco-6753651837110104.5-2xa.gif"
+  },
+  {
+    name: "doodle-haloowee",
+    link: "https://www.google.com/logos/doodles/2023/halloween-2023-6753651837109958-2xa.gif"
+  },
+  {
+    name: "doodle-4",
+    link: "https://www.cnet.com/a/img/resize/1df91c0ca563d765c8b92b13a94daa952b38fff8/hub/2016/09/14/53a4cb0b-f7ce-4934-9802-3e3d265ed495/doodle-4-google-logo.jpg?auto=webp&fit=crop&height=675&width=1200"
+  },
+  {
+    name: "doodle-edu",
+    link: "https://media-cdn-v2.laodong.vn/Storage/NewsPortal/2020/4/14/798210/Google-Doodle-4-9-20.jpg"
+  },
+  {
+    name: "asdasd",
+    link: "https://images.hindustantimes.com/img/2022/02/14/1600x900/google_doodle_1644804981291_1644804985296.PNG"
+  },
+  {
+    name: "Women",
+    link: "https://res.cloudinary.com/dqlelya6o/image/upload/v1709967697/vpryghufdwrpwskkkecb.png"
+  },
+  {
+    name: "VNC-6",
+    link: "https://res.cloudinary.com/dqlelya6o/image/upload/v1710138814/xo4koflcmcd7hzyq8rue.png"
+  },
+  {
+    name: "VNC-5",
+    link: "https://res.cloudinary.com/dqlelya6o/image/upload/v1710139163/rm2wxtmewmwjqwksoiqm.png"
+  },
+  {
+    name: "HCM",
+    link: "https://google-doodle-project.s3.ap-southeast-1.amazonaws.com/doodle/13.%2019-05.png?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEMP%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaDmFwLXNvdXRoZWFzdC0xIkYwRAIgOp%2B96cCjBMsINp7qRZzXYYxamUDYtgb6c3q0gDvNvzQCIFQYgt8a%2BD4x098E98D6wDCTo%2F2Uo3mtw14odZq6P41BKu0CCMz%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEQABoMODUxNzI1NTY5ODQzIgxWMIkcqzNEHhaAWLAqwQIlaze7zoN%2FjQpQCZDlEglmLB5pwI8RZk0yBTIsWJdy2H8VvGy%2FZNAtL1KdwBFqfgs7KZsOoALIQNkE6OiOLhXIfGWo77PjGkqdq4mXmr%2Bs5a%2BCm2RwoWL420yXih%2Fhh8ApBqehLY%2BGDrXW2b4SW9Lq4vO2guuYZOcqJ6OGg05ruidhTXyk8bV2qI%2BLOBdhEE0lIBrZ3GdCFc3%2BJyrI8RMkpFAl37nvTtHSO2cSDmhf1JshFxM6I8hIMKqM2FiO1kkiuaUFpdez%2FAUGHdzCPfOqaCl0%2BZb2ABJclpg1LXvME%2B8nrVE6xficquPtRSISRjRtHjkrIRvnmyPPk0Xg1sbnblqzpzteEuk4iosm5%2FlQB29RVzCjiOti7RUUEbTjSO0QoAKcQha9X51qB%2FAjOBUfcQDP1C9iMCTNL10SH9ZZXa0wveXerwY6tALOs85Li7fp1RKXIW38xHxPFsx%2BKMyMTiSFRl%2Ba87wemn9rUCJGWNNzhLQTbMesdWl7%2BvxPb6H%2BTnPCqeyHwtW0OJKrvaBYeerh7TjhwF24E0GdRi77OIDAHceS5CDA8bPRMt86KncZU70QiB7fvmH3U%2FMOSWdKXHME4pd335nSSDb2sypMryN7tBMji1fdA7G6bq8dC7BDtKlV%2BexGupauiYc%2Bf9XlFweSgsj9TyNJMJN8Gc8yn2VVhfYmPPJXyixURm17tFXwhkKXerRvK07AFFPPuQEo9a45oYNiA1kbGn4PSzU1nLvmHs%2BtKBq9fz0EGhQodKmJ1XN6GEGgQLdgC8RnEIGxToY5cmZBWq0RU7jMLfAwzj%2FD6JjaT9OcQ1h3UUPktZmhiJNMYk6FZQW3DHVlAg%3D%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20240318T075701Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Credential=ASIA4MTWMZ4ZR4D3AJXV%2F20240318%2Fap-southeast-1%2Fs3%2Faws4_request&X-Amz-Signature=ceb91bb44a80e4b5c825fe50028a3659d49d5aac2d8d0841cc02c8a1a9af3ddf"
+  },
+  {
+    name: "Love",
+    link: "https://res.cloudinary.com/dqlelya6o/image/upload/v1710316352/bnxzhzgf5bgbwvunimkn.png"
+  },
+  {
+    name: "30-04",
+    link: "https://google-doodle-project.s3.ap-southeast-1.amazonaws.com/doodle/10.%2030-4.png?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEMP%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaDmFwLXNvdXRoZWFzdC0xIkYwRAIgOp%2B96cCjBMsINp7qRZzXYYxamUDYtgb6c3q0gDvNvzQCIFQYgt8a%2BD4x098E98D6wDCTo%2F2Uo3mtw14odZq6P41BKu0CCMz%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEQABoMODUxNzI1NTY5ODQzIgxWMIkcqzNEHhaAWLAqwQIlaze7zoN%2FjQpQCZDlEglmLB5pwI8RZk0yBTIsWJdy2H8VvGy%2FZNAtL1KdwBFqfgs7KZsOoALIQNkE6OiOLhXIfGWo77PjGkqdq4mXmr%2Bs5a%2BCm2RwoWL420yXih%2Fhh8ApBqehLY%2BGDrXW2b4SW9Lq4vO2guuYZOcqJ6OGg05ruidhTXyk8bV2qI%2BLOBdhEE0lIBrZ3GdCFc3%2BJyrI8RMkpFAl37nvTtHSO2cSDmhf1JshFxM6I8hIMKqM2FiO1kkiuaUFpdez%2FAUGHdzCPfOqaCl0%2BZb2ABJclpg1LXvME%2B8nrVE6xficquPtRSISRjRtHjkrIRvnmyPPk0Xg1sbnblqzpzteEuk4iosm5%2FlQB29RVzCjiOti7RUUEbTjSO0QoAKcQha9X51qB%2FAjOBUfcQDP1C9iMCTNL10SH9ZZXa0wveXerwY6tALOs85Li7fp1RKXIW38xHxPFsx%2BKMyMTiSFRl%2Ba87wemn9rUCJGWNNzhLQTbMesdWl7%2BvxPb6H%2BTnPCqeyHwtW0OJKrvaBYeerh7TjhwF24E0GdRi77OIDAHceS5CDA8bPRMt86KncZU70QiB7fvmH3U%2FMOSWdKXHME4pd335nSSDb2sypMryN7tBMji1fdA7G6bq8dC7BDtKlV%2BexGupauiYc%2Bf9XlFweSgsj9TyNJMJN8Gc8yn2VVhfYmPPJXyixURm17tFXwhkKXerRvK07AFFPPuQEo9a45oYNiA1kbGn4PSzU1nLvmHs%2BtKBq9fz0EGhQodKmJ1XN6GEGgQLdgC8RnEIGxToY5cmZBWq0RU7jMLfAwzj%2FD6JjaT9OcQ1h3UUPktZmhiJNMYk6FZQW3DHVlAg%3D%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20240318T075524Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Credential=ASIA4MTWMZ4ZR4D3AJXV%2F20240318%2Fap-southeast-1%2Fs3%2Faws4_request&X-Amz-Signature=ac865d5bee836239c28ae9dd52c635383ec11c0ae404d418594f5595a6244aad"
+  },
+  {
+    name: "24-03",
+    link: "https://google-doodle-project.s3.ap-southeast-1.amazonaws.com/doodle/28.%2012-12.gif?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEMP%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaDmFwLXNvdXRoZWFzdC0xIkYwRAIgOp%2B96cCjBMsINp7qRZzXYYxamUDYtgb6c3q0gDvNvzQCIFQYgt8a%2BD4x098E98D6wDCTo%2F2Uo3mtw14odZq6P41BKu0CCMz%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEQABoMODUxNzI1NTY5ODQzIgxWMIkcqzNEHhaAWLAqwQIlaze7zoN%2FjQpQCZDlEglmLB5pwI8RZk0yBTIsWJdy2H8VvGy%2FZNAtL1KdwBFqfgs7KZsOoALIQNkE6OiOLhXIfGWo77PjGkqdq4mXmr%2Bs5a%2BCm2RwoWL420yXih%2Fhh8ApBqehLY%2BGDrXW2b4SW9Lq4vO2guuYZOcqJ6OGg05ruidhTXyk8bV2qI%2BLOBdhEE0lIBrZ3GdCFc3%2BJyrI8RMkpFAl37nvTtHSO2cSDmhf1JshFxM6I8hIMKqM2FiO1kkiuaUFpdez%2FAUGHdzCPfOqaCl0%2BZb2ABJclpg1LXvME%2B8nrVE6xficquPtRSISRjRtHjkrIRvnmyPPk0Xg1sbnblqzpzteEuk4iosm5%2FlQB29RVzCjiOti7RUUEbTjSO0QoAKcQha9X51qB%2FAjOBUfcQDP1C9iMCTNL10SH9ZZXa0wveXerwY6tALOs85Li7fp1RKXIW38xHxPFsx%2BKMyMTiSFRl%2Ba87wemn9rUCJGWNNzhLQTbMesdWl7%2BvxPb6H%2BTnPCqeyHwtW0OJKrvaBYeerh7TjhwF24E0GdRi77OIDAHceS5CDA8bPRMt86KncZU70QiB7fvmH3U%2FMOSWdKXHME4pd335nSSDb2sypMryN7tBMji1fdA7G6bq8dC7BDtKlV%2BexGupauiYc%2Bf9XlFweSgsj9TyNJMJN8Gc8yn2VVhfYmPPJXyixURm17tFXwhkKXerRvK07AFFPPuQEo9a45oYNiA1kbGn4PSzU1nLvmHs%2BtKBq9fz0EGhQodKmJ1XN6GEGgQLdgC8RnEIGxToY5cmZBWq0RU7jMLfAwzj%2FD6JjaT9OcQ1h3UUPktZmhiJNMYk6FZQW3DHVlAg%3D%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20240318T081039Z&X-Amz-SignedHeaders=host&X-Amz-Expires=299&X-Amz-Credential=ASIA4MTWMZ4ZR4D3AJXV%2F20240318%2Fap-southeast-1%2Fs3%2Faws4_request&X-Amz-Signature=6b6def1962f1f34e717c1d8189fe2a7145d48e912649e3529fa3ab5b83b71233"
+  }
+]
+console.log(JSON.stringify(listDoodle));
+const doodleListEle = document.querySelector(".doodle-list")
+const doodlePreview = document.querySelector(".doodle-preview")
+listDoodle.forEach((item) => {
+  let doodle = `<div class="doodle">
+  <img class = "ele" src="${item.link}" alt="">
+  <p class="title-doodle">${item.name}</p>
+  </div>`
+  doodleListEle.innerHTML += doodle;
+})
+
+let linkDoodle = ""
+let doodleList = document.querySelectorAll(".doodle")
+let currentIndexDoodle = 0;
+let nextLinkDoodle = ""
+const changeBtn = document.querySelector("#changeDoodle");
+const timer = document.querySelector("#timer")
+doodleList.forEach((item) => {
+  item.addEventListener('click', () => {
+    doodleList.forEach((it) => {
+      if (it != item) {
+        it.classList.remove("check")
+      }
+    })
+  
+    item.classList.add("check");
+    let imgDoodle = item.querySelector("img");
+    linkDoodle = imgDoodle.getAttribute("src");
+    doodlePreview.innerHTML = `<img class = "ele-preview" src="${linkDoodle}" alt=""></img>`
+    console.log(imgDoodle.outerHTML);
+    listDoodle.forEach((item) => {
+      if (item.link == linkDoodle) {
+        currentIndexDoodle = listDoodle.indexOf(item)
+        nextLinkDoodle = listDoodle[(currentIndexDoodle + 1) % listDoodle.length].link;
+        console.log(currentIndexDoodle);
+        console.log(nextLinkDoodle)
+      }
+    })
+  })
+})
+
+function sendMessageToContentScript(message) {
+  return new Promise((resolve) => {
+    chrome.tabs.query({
+      active: true,
+      currentWindow: true
+    }, (tabs) => {
+      console.log("Tabs:", tabs);
+      const activeTab = tabs[0];
+      chrome.tabs.sendMessage(activeTab.id, {
+        link: message
+      }, () => {
+        resolve();
+      });
+    });
+  });
+}
+
+// setInterval(() => {
+//   sendMessageToContentScript(linkDoodle);
+//   currentIndexDoodle = (currentIndexDoodle + 1) % listDoodle.length;
+//   nextLinkDoodle = listDoodle[currentIndexDoodle].link;
+//   linkDoodle = nextLinkDoodle;
+// }, 1000)
+
+changeBtn.addEventListener("click", (event) => {
+  console.log(timer.value);
+  if (timer.value) {
+    setInterval(() => {
+      sendMessageToContentScript(linkDoodle);
+      currentIndexDoodle = (currentIndexDoodle + 1) % listDoodle.length;
+      nextLinkDoodle = listDoodle[currentIndexDoodle].link;
+      linkDoodle = nextLinkDoodle;
+      console.log(linkDoodle);
+    }, timer.value * 1000)
+  } else {
+    sendMessageToContentScript(linkDoodle);
+  }
+  event.preventDefault()
+})
+
+// changeBtn.addEventListener('click', async () => {
+//   await sendMessageToContentScript(linkDoodle);
+// });
+
+// changeBtn.addEventListener('click', async () => {
+//   // Kiểm tra giá trị timer hợp lệ
+//   if (timer.value) {
+//     const timerValue = parseInt(timer.value);
+//     // Lấy Doodle được chọn
+//     const selectedDoodle = document.querySelector(".doodle.check");
+//     if (!selectedDoodle) {
+//       // Xử lý trường hợp chưa chọn Doodle (ví dụ: hiển thị thông báo)
+//       return;
+//     }
+
+//     currentIndexDoodle = listDoodle.indexOf(selectedDoodle);
+//     nextLinkDoodle = listDoodle[(currentIndexDoodle + 1) % listDoodle.length].link;
+//     await sendMessageToContentScript(selectedDoodle.querySelector("img").getAttribute("src"));
+//     const intervalId = setInterval(async () => {
+//       await sendMessageToContentScript(nextLinkDoodle);
+//       currentIndexDoodle = (currentIndexDoodle + 1) % listDoodle.length;
+//       nextLinkDoodle = listDoodle[currentIndexDoodle].link;
+//     }, timerValue * 1000);
+
+//     // Xóa interval khi click nút lần nữa
+//     changeBtn.addEventListener('click', () => clearInterval(intervalId));
+//   } else {
+//     await sendMessageToContentScript(linkDoodle);
+//     return;
+//   }
+// });
